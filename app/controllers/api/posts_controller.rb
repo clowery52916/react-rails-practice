885f705b21a4016872fd1e4a21fdc51dd1d41283
crmgunter <crmgunter@gmail.com>
2018-04-02 15:28:02 -0400

    got post route working on back end

--- app/controllers/api/posts_controller.rb
+++ app/controllers/api/posts_controller.rb
@@ -5,9 +5,26 @@ class Api::PostsController < ApplicationController
         render json: @posts
     end
 
+    def create
+        @city = City.find(params[:city_id])
+        @new_post = @city.posts.create!(post_params)
+        render json: {
+            post: @new_post
+        }
+
+        # redirect_to ("/cities/")
+    end
+
+
     def show
         @post = Post.find(params[:id])
         render json: @post
+    end
+
+    private
+
+    def post_params
+        params.require(:post).permit(:title, :comment, :city_id)
     end
 
 end
