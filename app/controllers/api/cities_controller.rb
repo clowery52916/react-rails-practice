bfa4f0bdc3f3c9928d011988553a1609d6f0f02d
crmgunter <crmgunter@gmail.com>
2018-04-02 11:18:55 -0400

    deployed to heroku

--- app/controllers/api/cities_controller.rb
+++ app/controllers/api/cities_controller.rb
@@ -4,8 +4,20 @@ class Api::CitiesController < ApplicationController
         render json: @cities
     end
 
+    def create
+        @city = City.create!(city_params)
+
+        render json: @city
+    end
+
     def show
         @city = City.find(params[:id])
         render json: @city
+    end
+
+
+    private 
+    def city_params
+        params.require(:city).permit(:name, :photo_url, :description, :post)
     end
 end
