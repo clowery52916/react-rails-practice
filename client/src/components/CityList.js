499d1fcbdfdb5af4abf44afe428726e73c507a5d
crmgunter <crmgunter@gmail.com>
2018-04-01 20:06:15 -0400

    setting up react side, preparing for API calls, god help me

---
+++ client/src/components/CityList.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class CityList extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Whaddup</h1>
+            </div>
+        );
+    }
+}
+
+export default CityList;
