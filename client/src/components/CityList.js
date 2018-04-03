e6be412b7aac8565d37c53eb21a4e8d5c5d65cbb
clowery <clowery52916@gmail.com>
2018-04-03 12:26:33 -0400

    styled CityList page

--- client/src/components/CityList.js
+++ client/src/components/CityList.js
@@ -1,6 +1,35 @@
 import React, {Component} from 'react';
 import axios from 'axios'
 import {Link} from 'react-router-dom'
+import { Image } from 'semantic-ui-react'
+import styled from 'styled-components'
+
+
+const ImageContainer = styled.img`
+    max-width: 60vw;
+    border-radius: 20px;
+    image-orientation: center;
+
+    flex-basis: auto;
+
+
+
+`;
+const CityContainer = styled.div`
+  text-align: center;
+  overflow-y: scroll;
+  overflow-x:
+   hidden;
+`;
+
+const LinkContainer = styled.div `
+  font-size: 20px;
+`
+
+
+
+
+
 
 class CityList extends Component {
   state = {
@@ -28,18 +57,30 @@ class CityList extends Component {
     if (this.state.error) {
       return <div>{this.state.error}</div>
     }
-    return (<div>
-      <h1>View All the Cities!</h1>
+    return (
+      <CityContainer>
+        <br/>
+      <h1>View All Cities</h1>
       {console.log(this.state.cities)}
       {
         this.state.cities.map(city => (<div key={city.id}>
+          <br/>
+          <LinkContainer>
           <Link to={`/cities/${city.id}`}>
             <p>{city.name}</p>
+            <br/>
           </Link>
-          <img src={city.img}/>
+          </LinkContainer>
+          {/* <ImageContainer> */}
+          <ImageContainer src={city.img}/>
+          {/* </ImageContainer> */}
+          <br/>
+          <br/>
+
         </div>))
       }
-    </div>);
+    </CityContainer>);
+
   }
 }
 
